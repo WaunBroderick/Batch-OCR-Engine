62762bdf335d1fb559c7d8a88d7f5888ddac7952
Waun Broderick <waunbroderick@gmail.com>
2018-11-22 02:49:21 -0500

    HOTFIX attempt

--- Main.py
+++ Main.py
@@ -1,6 +1,9 @@
-import matplotlib
-# Force matplotlib to not use any Xwindows backend.
-matplotlib.use('Agg')
+import os
+import matplotlib as mpl
+if os.environ.get('DISPLAY','') == '':
+    print('no display found. Using non-interactive Agg backend')
+    mpl.use('Agg')
+import matplotlib.pyplot as plt
 
 #####################DO NOT REMOVE###############################
 ##                                                             ##
